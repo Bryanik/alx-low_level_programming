5a550b136446b725a6560977705011f329e5602f
Bryanik <bikechukwu2@gmail.com>
2023-01-17 20:39:58 -0800

    Fixed Seg Bug

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,13 @@
 
 int main(int argc, char **argv)
 {
-	int mul = atoi(argv[1]) * atoi(argv[2]);
+	int mul;
 
 	if (argc > 2)
 	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
+	
 		return (0);
 	}
 	else
