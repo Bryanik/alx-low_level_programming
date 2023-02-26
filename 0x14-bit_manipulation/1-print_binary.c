2b1911441078278f3197c7ec1c2328ca428c31d5
Bryanik <bikechukwu2@gmail.com>
2023-02-26 15:38:32 -0800

    Bit Manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,6 +3,10 @@
  * _power - calculate (base and power)
  * @base: base of the exponet
  * @pow: power of the exponet
+ *
+ * Description: _power calculates the base^highest_index
+ * eg for base 2 stored in 1 byte the result is 2^7
+ * with binary rep 1000 0000
  * Return: value of base and power
  */
 unsigned long int _power(unsigned int base, unsigned int pow)
@@ -25,6 +29,7 @@ void print_binary(unsigned long int n)
 	unsigned long int dev, result;
 	char flag;
 
+	/*Used to track when the first non-zero digit was printed*/
 	flag = 0;
 	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
 
@@ -37,6 +42,11 @@ void print_binary(unsigned long int n)
 			_putchar('1');
 
 		}
+		/*
+		 * flag == 1 indicates the number has started,
+		 * therefore if result != dev print 0
+		 * dev == 1 enables the program print 0 at the final dev if n = 0
+		 */
 		else if (flag == 1 || dev == 1)
 		{
 			_putchar('0');
